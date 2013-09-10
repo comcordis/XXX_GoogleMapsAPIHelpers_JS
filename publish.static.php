0573010b576bdfd67b730b7acec47348a0a74165
vinceMeens <v.w.a.meens@comcordis.com>
2013-09-11 01:27:58 +0200

    Static publishing per project

---
+++ publish.static.php
@@ -0,0 +1,15 @@
+<?php
+
+XXX_Static_Publisher::publishMergedFilesFromOtherProject($project, $deployIdentifier, array
+(
+	'XXX_GoogleMapsAPI_GeocoderService.js',
+	'XXX_GoogleMapsAPI_DistanceMatrixService.js',
+	'XXX_GoogleMapsAPI_PlacesService.js',
+	'XXX_GoogleMapsAPI_Geometry.js',
+	'XXX_GoogleMapsAPI_GeocoderSuggestionSource.js',
+	'XXX_GoogleMapsAPI_PlacesSuggestionSource.js',
+	'XXX_GoogleMapsAPI_MapPositionDragger.js'
+),
+'initialize.js');
+
+?>
